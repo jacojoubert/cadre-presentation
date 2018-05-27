82db723904ea6c445b8aa405f47cc40fd76c57ad
Jaco Joubert <joubert.jaco@gmail.com>
2018-05-27 15:19:23 -0400

    Stub in routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,26 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('welcome');
+  this.route('goals');
+  this.route('problem', function() {
+    this.route('features');
+    this.route('ui-button');
+  });
+  this.route('approach', function() {
+    this.route('theming');
+    this.route('variant');
+    this.route('blueprint');
+    this.route('layouts');
+    this.route('ui-section');
+  });
+  this.route('demo', function() {
+    this.route('ui-form');
+    this.route('ui-select-date');
+    this.route('ui-modal');
+    this.route('ui-popover');
+    this.route('ui-other');
+  });
 });
 
 export default Router;
