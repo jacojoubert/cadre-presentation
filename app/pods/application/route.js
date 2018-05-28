9c680c2f17295ffd6419f1b15fb7462a214738d1
Jaco Joubert <joubert.jaco@gmail.com>
2018-05-28 15:27:01 -0400

    Add stage support

---
+++ app/pods/application/route.js
@@ -0,0 +1,70 @@
+import Route from "@ember/routing/route";
+import { inject as service } from '@ember/service';
+
+export default Route.extend({
+  router: service(),
+
+  order: [
+    'index',
+    'problem.features',
+    'problem.ui-button',
+    'approach.theming',
+    'approach.variant',
+    'approach.blueprint',
+    'approach.layouts',
+    'approach.ui-section',
+    'demo.ui-form',
+    'demo.ui-select-date',
+    'demo.ui-modal',
+    'demo.ui-popover',
+    'demo.ui-other'
+  ],
+
+  keyUp(event) {
+    const current = this.get('router.currentRouteName');
+    const order = this.get('order');
+    const currentIndex = order.indexOf(current);
+
+    // Right
+    if (event.which === 39) {
+      let currentController = this.controllerFor(current);
+      let next = order[currentIndex + 1];
+      try {
+        let nextController = this.controllerFor(next);
+      } catch(error) {
+
+      }
+
+      if (currentController.get('stage') < currentController.get('maxStage')) {
+        currentController.incrementProperty('stage');
+
+      } else {
+        this.get('router').transitionTo(next);
+      }
+    }
+
+    // Left
+    if (event.which === 37) {
+      let currentController = this.controllerFor(current);
+      let previous = order[currentIndex - 1];
+      try {
+        let previousController = this.controllerFor(previous);
+      } catch(error) {
+
+      }
+
+      if (currentController.get('stage') > 1) {
+        currentController.decrementProperty('stage');
+
+      } else {
+        this.get('router').transitionTo(previous);
+      }
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    this.keyUp = this.keyUp.bind(this);
+    document.body.addEventListener('keyup', this.keyUp, true);
+  }
+});
